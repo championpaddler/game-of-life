44260d620821a01522cbaacd44c1b5ba091684bd
championpaddler <sk9331657@gmail.com>
2025-05-25 12:47:19 +0530

    Add Models

---
+++ com/chargepoint/gof/model/Cell.java
@@ -0,0 +1,23 @@
+
+package com.chargepoint.gof.model;
+
+public class Cell {
+    private boolean alive;
+
+    public Cell(boolean alive) {
+        this.alive = alive;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    @Override
+    public String toString() {
+        return alive ? "O" : ".";
+    }
+}
